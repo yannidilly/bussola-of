ac821b805a9b71eb9b889fd5b242a22dda0b185d
Yanni <yannidilly.prog@gmail.com>
2023-05-29 09:44:21 -0300

    cria componente de pergunta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Question from "./components/Question";
+
 function App() {
   return (
     <div className="App">
       <h1>Bússola Financeira</h1>
+      <Question />
     </div>
   );
 }
---
+++ src/components/Question.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Question extends React.Component {
+  render() {
+    return (
+      <h2>Pergunta</h2>
+    );
+  }
+}
+
+export default Question;
