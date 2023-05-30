df0203ea23d909064a504cb6346a58d7509f10c2
Yanni <yannidilly.prog@gmail.com>
2023-05-30 09:11:41 -0300

    adiciona GoogleFormProvider encapsulando o form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { GoogleFormProvider, useGoogleForm } from 'react-google-forms-hooks'
+import form from '../utils/googleForm.json';
 import Questions from './Questions';
 
 function Form() {
+  const methods = useGoogleForm({ form })
+  const onSubmit = async (data) => {
+    console.log('>>> Here is the data', data)
+    await methods.submitToGoogleForms(data)
+    alert('Form submitted with success!')
+  }
 
   return (
-    <form>
-      <Questions />
-    </form>
+    <GoogleFormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <Questions />
+        <button type='submit'>Finalizar</button>
+      </form>
+    </GoogleFormProvider>
   );
 }
 
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -4,6 +4,7 @@ import FormContext from '../context/FormContext';
 
 function Question() {
   const { form } = useContext(FormContext);
+  // adicionar useLinearInput do react-google-forms-hooks
 
   return (
     <div className='questions'>
@@ -13,7 +14,7 @@ function Question() {
             <h2>{ field.label }</h2>
             { field.options.map((option) => (
                 <div key={ option.label }>
-                  <input type="radio" id={ option.label } name='currentQuestion' value={ option.label }></input>
+                  <input type="radio" id={ option.label } name={ field.id } value={ option.label } />
                   <label>{ option.label }</label>
                 </div>
               )) }
