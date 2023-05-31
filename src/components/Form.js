18e0e969e586b53fb6a2049d29f6ff9329c3c9c7
Yanni <yannidilly.prog@gmail.com>
2023-05-31 18:36:11 -0300

    adiciona tela para finalizar questionário após terminar as perguntas

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react'
 import { GoogleFormProvider, useGoogleForm } from 'react-google-forms-hooks'
 import form from '../utils/googleForm.json';
+import QuestionContext from '../context/QuestionContext';
 import Questions from './Questions';
 
 function Form() {
+  const { question: { currentIndex } } = useContext(QuestionContext);
+
   const methods = useGoogleForm({ form })
   const onSubmit = async (data) => {
     console.log('>>> Here is the data', data)
@@ -15,7 +18,18 @@ function Form() {
     <GoogleFormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(onSubmit)}>
         <Questions />
-        <button type='submit'>Finalizar</button>
+        <div>
+          {
+            (currentIndex > 36) ? (
+              <div>
+                <p>Você terminou o teste</p>
+                <button type='submit'>Finalizar</button>
+              </div>
+            ) : (
+              <></>
+            )
+          }
+        </div>
       </form>
     </GoogleFormProvider>
   );
