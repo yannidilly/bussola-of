100348b540e9f9fc7d52a5325292fc04628abb93
Yanni <yannidilly.prog@gmail.com>
2023-06-02 12:19:37 -0300

    torna renderização do input de pergunta dinâmico

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -2,11 +2,28 @@ import React from 'react';
 import LinearScaleQuestion from './LinearScaleQuestion';
 
 function Question({ fields }) {
+  const fieldType = (field, index) => {
+    let input = <></>;
+    switch (field.type) {
+      case "LINEAR":
+        input = <LinearScaleQuestion id={field.id} index={ index } />
+        break;
+    
+      default:
+        input = <></>
+        break;
+    }
+
+    return input;
+  }
+
   return (
     <div className='questions'>
       {
         fields.map((field, index) => (
-          <LinearScaleQuestion id={field.id} key={field.id} index={ index } />
+          <div key={index}>
+            {fieldType(field, index)}
+          </div>
         ))
       }
     </div>
