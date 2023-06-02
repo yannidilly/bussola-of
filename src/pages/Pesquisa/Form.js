c43db231bba4fa566a865dbeea564ac1944334b7
Yanni <yannidilly.prog@gmail.com>
2023-06-02 12:54:39 -0300

    separa tela final das perguntas

--- src/pages/Pesquisa/Form.js
+++ src/pages/Pesquisa/Form.js
@@ -3,8 +3,10 @@ import { GoogleFormProvider, useGoogleForm } from 'react-google-forms-hooks';
 import pesquisaFormData from '../../utils/pesquisaForm.json';
 import Questions from '../../components/Questions';
 import PesquisaContext from '../../context/PesquisaContext';
+import QuestionContext from '../../context/QuestionContext';
 
 function Form() {
+  const { question: { currentIndex } } = useContext(QuestionContext);
   const { pesquisaForm: { fields } } = useContext(PesquisaContext);
 
   const methods = useGoogleForm({ form: pesquisaFormData });
@@ -18,7 +20,16 @@ function Form() {
     <GoogleFormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(onSubmit)}>
         <Questions fields={ fields }/>
-        <button className='finish-bussola-form-button' type='submit'>Finalizar</button>
+        {
+          (currentIndex >= fields.length) ? (
+            <div>
+              <p>Você terminou todas as perguntas, clique no botão abaixo para ver seu resultado. Obs: ao clicar no botão abaixo você concorda com nossa política de privacidade (link)</p>
+              <button className='finish-bussola-form-button' type='submit'>Finalizar</button>
+            </div>
+          ) : (
+            <></>
+          )
+        }
       </form>
     </GoogleFormProvider>
   )
