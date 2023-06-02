2ddfcc418e8ddaa0bc674611210c7dd54c538d7e
Yanni <yannidilly.prog@gmail.com>
2023-06-02 11:59:56 -0300

    implementa context para pesquisa

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
 import bussolaFormData from './utils/bussolaForm.json';
-import QuestionContext from './context/QuestionContext';
+import pesquisaFormData from './utils/pesquisaForm.json';
 import BussolaContext from './context/BussolaContext';
+import PesquisaContext from './context/PesquisaContext';
+import QuestionContext from './context/QuestionContext';
 import Pages from './pages';
 
 function App() {
   const [bussolaForm, setbussolaForm] = useState({ ...bussolaFormData });
+  const [pesquisaForm, setPesquisaForm] = useState({ ...pesquisaFormData });
   const [question, setQuestion] = useState({ currentIndex: 0 });
 
   return (
-    <BussolaContext.Provider value={ { bussolaForm, setbussolaForm } }>
-      <QuestionContext.Provider value={ { question, setQuestion } }>
-        <Pages />
-      </QuestionContext.Provider>
-    </BussolaContext.Provider>
+    <PesquisaContext.Provider value={ { pesquisaForm, setPesquisaForm } }>
+      <BussolaContext.Provider value={ { bussolaForm, setbussolaForm } }>
+        <QuestionContext.Provider value={ { question, setQuestion } }>
+          <Pages />
+        </QuestionContext.Provider>
+      </BussolaContext.Provider>
+    </PesquisaContext.Provider>
   );
 }
 
