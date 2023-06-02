0e26b43d748da71c5c6ee4af2d6a82ec6ca7fa40
Yanni <yannidilly.prog@gmail.com>
2023-06-02 14:27:35 -0300

    implementa componente de resposta curta

--- src/components/inputs/ShortAnswer.js
+++ src/components/inputs/ShortAnswer.js
@@ -1,16 +1,32 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { useShortAnswerInput } from 'react-google-forms-hooks';
 import QuestionContext from '../../context/QuestionContext';
 import ControlFormButtons from '../ControlFormButtons';
 
 function ShortAnswer({ id, index }) {
   const { question: { currentIndex } } = useContext(QuestionContext);
+  const [filled, setFilled] = useState(false);
+
+  const { label, register, required } = useShortAnswerInput(id);
+
+  const onInputChange = (event) => {
+    // a ordem do onChange no componente importa
+    if (event.target.value.length > 5) {
+      setFilled(true);
+    }
+  };
 
   const visible = (index === currentIndex) ? '' : 'none';
 
   return (
     <div style={ { display: visible } }>
-      <p>ShortAnswer</p>
-      <ControlFormButtons />
+      <h2>{label}</h2>
+      <input
+        type='text'
+        {...register()}
+        onChange={ onInputChange }
+        />
+      <ControlFormButtons disable={ !(filled || !required) }/>
     </div>
   )
 }
