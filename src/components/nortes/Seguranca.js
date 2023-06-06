11b722fb5feb1ff034a66ab0106739ebe4c0bd55
Yanni <yannidilly.prog@gmail.com>
2023-06-06 18:09:48 -0300

    finaliza criação de páginas de norte

--- src/components/nortes/Seguranca.js
+++ src/components/nortes/Seguranca.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Points from "../Points";
+import SegurancaPDF from '../../pdfsNorte/Seguranca.pdf';
 
 function Seguranca() {
   return (
-    <h1>Segurança</h1>
+    <div className="norte-seguranca">
+      <h1>Segurança</h1>
+      <div className="video-bussola">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ZiE5zjxwstI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
+      <Points />
+      <a href={ SegurancaPDF } target="_blank" rel="noreferrer">Baixar resumo</a>
+    </div>
   );
 }
 
